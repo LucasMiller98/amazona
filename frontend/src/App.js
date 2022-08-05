84a133d370e66ed06d005f43b864eda6352e769f
LucasMiller98 <lucas.miller1998@outlook.com>
2022-08-05 19:19:14 -0300

    Amazona

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
-import { Link } from 'react-router-dom'
+import { data } from './data';
+import { object } from 'prop-types'
 
 function App() {
+
+  const { products } = data
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -8,10 +12,35 @@ function App() {
       </header>
 
       <main>
-        list products
+        <h1>Featured Products</h1>
+
+        <div className='products'>
+          { products.map(product => (
+              <div className='product' key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                
+                <div className='product-info'>
+                  <a href={`/product/${product.slug}`}>
+                    <p>{ product.name }</p>
+                  </a>
+                  
+                  <p><strong>{ product.price }</strong></p>
+
+                  <button type='button'>Add to cart</button>
+                </div>
+              </div>
+          ))}
+        </div>
+
       </main>
     </div>
   );
 }
 
 export default App;
+
+App.propTypes = {
+  data: object
+}
