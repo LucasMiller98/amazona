c256485f050652e6255803d61482f4116be919e6
LucasMiller98 <lucas.miller1998@outlook.com>
2022-08-05 12:40:59 -0300

    E-commerce

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href='/'>amazona</a>
+      </header>
+
+      <main>
+        list products
+      </main>
+    </div>
+  );
+}
+
+export default App;
