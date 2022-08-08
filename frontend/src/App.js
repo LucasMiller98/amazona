283a30c115122b926eabd531ca120f14c515d7c8
LucasMiller98 <lucas.miller1998@outlook.com>
2022-08-07 23:23:17 -0300

    E-commerce

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { HomeScreen  } from './pages/HomeScreen';
 import { ProductScreen } from './pages/ProductScreen'
+import { Container, Navbar } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 export function App() {
 
@@ -8,18 +10,30 @@ export function App() {
   return (
     <BrowserRouter>
     
-      <div className="App">
-        <header className="App-header">
-          <Link to='/'>amazona</Link>
+      <div className='d-flex flex-column site-container'>
+        <header>
+          <Navbar bg='dark' variant='dark'>
+            <Container>
+              <LinkContainer to='/'>
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
 
         <main>
-          <Routes>
-            <Route path='/product/:slug' element={ <ProductScreen/>} />
-            <Route path='/' element={ <HomeScreen/> } />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path='/product/:slug' element={ <ProductScreen/>} />
+              <Route path='/' element={ <HomeScreen/> } />
+            </Routes>
+          </Container>
 
         </main>
+
+        <footer>
+          <div className='text-center'>All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
