8b678d935f442eaa788d834c8b2e1a5404d3a600
LucasMiller98 <lucas.miller1998@outlook.com>
2022-08-07 23:57:10 -0300

    E-commerce

---
+++ frontend/src/components/Product.js
@@ -0,0 +1,29 @@
+import { object } from 'prop-types'
+import { Button, Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+export function Product(props) {
+
+  const { product } = props
+
+  return (
+    <Card key={product.slug}>
+      <Link to={`/product/${product.slug}`}>
+        <img src={product.image} className='card-img-top' alt={product.name} />
+      </Link>
+      
+      <Card.Body>
+        <Link to={`/product/${product.slug}`}>
+          <Card.Title>{ product.name }</Card.Title>
+        </Link>
+        <Card.Text>${ product.price }</Card.Text>
+        <Button>Add to cart</Button>
+      </Card.Body>
+    </Card>
+  )
+  
+}
+
+Product.propTypes = {
+  product: object
+}
--- frontend/src/pages/HomeScreen.js
+++ frontend/src/pages/HomeScreen.js
@@ -1,9 +1,10 @@
 // import { data } from '../data';
 import { object, array, bool } from 'prop-types'
-import { Link } from 'react-router-dom';
 import { useEffect, useReducer } from 'react'
+import { Product }  from '../components/Product'
 import axios from 'axios';
 import logger from 'use-reducer-logger'
+import { Col, Row } from 'react-bootstrap';
 
 const reducer = (state, action) => {
   switch(action.type) {
@@ -63,24 +64,14 @@ export function HomeScreen() {
           ): error ? (
             <div>{ error }</div>
           ): (
-          
-            products.map(product => (
-              <div className='product' key={product.slug}>
-                <Link to={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </Link>
-                
-                <div className='product-info'>
-                  <Link to={`/product/${product.slug}`}>
-                    <p>{ product.name }</p>
-                  </Link>
-                  
-                  <p><strong>${ product.price }</strong></p>
-
-                  <button type='button'>Add to cart</button>
-                </div>
-              </div>
-          )))
+            <Row>
+              {products.map(product => (
+                <Col sm={6} md={4} lg={3} className='mb-3'>
+                  <Product product={product} />
+                </Col>
+              ))}
+            </Row>
+          )
         }
         
       </div>
